9510dea0f4d2ae1fa407e1f80b4648d5e4f2e2be
Daniel Jaimes <ingdev8023@gmail.com>
2023-11-01 20:23:10 -0500

    Proyecto terminado con todas las combinaciones

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our', 'such', 'that', 'these', 'this', 'those'];
+  let adj = ['great', 'big', 'attractive', 'bald','beautiful','chubby','clean',
+  'dazzling','drab','elegant','fancy','fit','glamorous','gorgeous','handsome','long',
+  'magnificent','short','skinny','stocky'];
+  let noun = ['jogger', 'racoon', 'Actor', 'Gold','Painting','Advertisement','Grass','Parrot','Afternoon',
+   'Pencil','Airport','Guitar','Piano','Pillow','Animal','Hamburger','Pizza','Answer','Helicopter','Planet',
+   'Apple','Helmet','Plastic','Army','Holiday'];
+  let extension = ['.com', '.us', '.net', '.io', '.gov']
+  let allTheDomains = [];
+  for(let i = 0; i < pronoun.length; i++){
+    for(let j = 0; j < adj.length; j++){
+      for(let k= 0; k < noun.length; k++){
+        for(let l = 0; l < extension.length; l++){
+          allTheDomains.push(pronoun[i]+adj[j]+noun[k]+extension[l])
+        }
+      }
+    }
+  }
+  console.log(allTheDomains);
 };
